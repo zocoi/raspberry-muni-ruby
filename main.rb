751fa145b3fea24613c4d5478a3cd5c58ba3e15b
Hung Dao <hung@groupon.com>
2014-06-02 14:32:59 -0700

    update

--- main.rb
+++ main.rb
@@ -17,34 +17,79 @@ class Main
     @index ||= default_index
     @route ||= default_route
 
-    Adafruit::LCD::Char16x2.new{|lcd|
+    @lcd = Adafruit::LCD::Char16x2.new{|lcd|
       lcd.clear
       lcd.backlight(Adafruit::LCD::Char16x2::WHITE)
       lcd.message(predictions())
-
-      while true
-        buttons = lcd.buttons
-        case
-        when (buttons >> Adafruit::LCD::Char16x2::SELECT) & 1 > 0
-          puts "SELECT pressed"
-        when (buttons >> Adafruit::LCD::Char16x2::LEFT) & 1 > 0
-          puts "LEFT pressed"
-        when (buttons >> Adafruit::LCD::Char16x2::RIGHT) & 1 > 0
-          puts "RIGHT pressed"
-        when (buttons >> Adafruit::LCD::Char16x2::UP) & 1 > 0
-          puts "UP pressed"
-          up()
-          lcd.clear
-          lcd.message(predictions())
-        when (buttons >> Adafruit::LCD::Char16x2::DOWN) & 1 > 0
-          puts "DOWN pressed"
-          down()
-          lcd.clear
-          lcd.message(predictions())
-        end
-        sleep 0.1
-      end
     }
+
+    while true
+      buttons = @lcd.buttons
+      case
+      when (buttons >> Adafruit::LCD::Char16x2::SELECT) & 1 > 0
+        puts "SELECT pressed"
+      when (buttons >> Adafruit::LCD::Char16x2::LEFT) & 1 > 0
+        puts "LEFT pressed"
+        left()
+      when (buttons >> Adafruit::LCD::Char16x2::RIGHT) & 1 > 0
+        puts "RIGHT pressed"
+        right()
+      when (buttons >> Adafruit::LCD::Char16x2::UP) & 1 > 0
+        puts "UP pressed"
+        up()
+        preparePredictions()
+      when (buttons >> Adafruit::LCD::Char16x2::DOWN) & 1 > 0
+        puts "DOWN pressed"
+        down()
+        preparePredictions()
+      end
+      sleep 0.1
+    end
+  end
+
+  def up()
+    @index = @index - 1
+    @route = @routes[index]
+  end
+
+  def down()
+    @index = @index + 1
+    @route = @routes[index]
+  end
+
+  # RED                     = 0x01
+  # GREEN                   = 0x02
+  # BLUE                    = 0x04
+  # YELLOW                  = RED + GREEN
+  # TEAL                    = GREEN + BLUE
+  # VIOLET                  = RED + BLUE
+  # WHITE                   = RED + GREEN + BLUE
+
+  def right()
+    lcd.backlight(Adafruit::LCD::Char16x2::WHITE)
+  end
+
+  def left()
+    lcd.backlight(Adafruit::LCD::Char16x2::TEAL)
+  end
+
+  def preparePredictions()
+    if @t
+      @t.kill
+    @t = Thread.new do
+       loop do
+         writePredictions()
+         sleep 2000
+       end
+     end
+  end
+
+  def writePredictions()
+    @lcd.clear
+    @lcd.message("Loading...")
+    str = predictions()
+    @lcd.clear
+    @lcd.message(str)
   end
 
   # Get a prediction
@@ -55,18 +100,6 @@ class Main
     str = str.scan(/.{1,16}/).join("\n")
     puts str
     str
-  end
-
-  def up()
-    @index = @index - 1
-    @route = @routes[index]
-    predictions()
-  end
-
-  def down()
-    @index = @index + 1
-    @route = @routes[index]
-    predictions()
   end
 end
 
