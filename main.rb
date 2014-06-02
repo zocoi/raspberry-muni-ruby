bc3733a0edb2725e5f54e14922d8a67a7f87346b
Hung Dao <hung@groupon.com>
2014-06-02 13:47:57 -0700

    updated outbound

--- main.rb
+++ main.rb
@@ -34,10 +34,12 @@ class Main
         when (buttons >> Adafruit::LCD::Char16x2::UP) & 1 > 0
           puts "UP pressed"
           up()
+          lcd.clear
           lcd.message(predictions())
         when (buttons >> Adafruit::LCD::Char16x2::DOWN) & 1 > 0
           puts "DOWN pressed"
           down()
+          lcd.clear
           lcd.message(predictions())
         end
         sleep 0.1
@@ -48,7 +50,7 @@ class Main
   # Get a prediction
   def predictions()
     @route = Muni::Route.find(@route.tag)
-    predictions = @route.inbound.stop_at(@location).predictions
+    predictions = @route.outbound.stop_at(@location).predictions
     str = "#{@route.title}:" + predictions.map(&:minutes).join(",")
     str = str.scan(/.{1,16}/).join("\n")
     puts str
@@ -68,30 +70,3 @@ class Main
   end
 end
 
-#
-# if __FILE__ == $0
-#
-#   Adafruit::LCD::Char16x2.new{|lcd|
-#     lcd.clear
-#     lcd.backlight(Adafruit::LCD::Char16x2::WHITE)
-#     lcd.message("life is like\nriding a bicycle")
-#
-#     while true
-#       buttons = lcd.buttons
-#       case
-#       when (buttons >> Adafruit::LCD::Char16x2::SELECT) & 1 > 0
-#         puts "SELECT pressed"
-#       when (buttons >> Adafruit::LCD::Char16x2::LEFT) & 1 > 0
-#         puts "LEFT pressed"
-#       when (buttons >> Adafruit::LCD::Char16x2::RIGHT) & 1 > 0
-#         puts "RIGHT pressed"
-#       when (buttons >> Adafruit::LCD::Char16x2::UP) & 1 > 0
-#         puts "UP pressed"
-#       when (buttons >> Adafruit::LCD::Char16x2::DOWN) & 1 > 0
-#         puts "DOWN pressed"
-#       end
-#       sleep 0.1
-#     end
-#   }
-#
-# end
